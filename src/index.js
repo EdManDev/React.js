ee140847528767b17683b92734b492500e6f04fa
EdManDev <ed.manigat@gmail.com>
2019-03-23 22:12:10 -0400

    update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
-import React from 'react';
+import  React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import "./index.css";
+import img from "./LOGO EDMAN 2018 copy.png";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Container () {
+  return(
+    <section>
+      <Img1/>
+      <Img2/>
+      <Title/>
+      <Subtitle/>
+      <Subtitle/>
+      <SomeText/>
+      <Footer/>
+    </section>
+  );
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Img1 = () => (
+  <img width="200" src={img} alt="img"/>
+);
+
+const Img2 = () => (
+  <img width="300" src={img} alt="img"/>
+);
+
+const Title = () => <h1 className="h1">hello this is the Title</h1>;
+
+const Subtitle = () => <p>hello this is the subtitle</p>;
+
+const Footer = () => <div className="footer"><p>© 2018 EdMan. All Rights Reserved</p></div>;
+
+const SomeText = () => <div className="sometext">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias nam vel fuga architecto consequatur distinctio eos. Autem aliquid natus adipisci aut architecto. Sapiente harum aliquam expedita distinctio, quasi dicta! Similique!</div>
+
+
+
+
+ReactDOM.render(<Container/>, document.getElementById("root"));
